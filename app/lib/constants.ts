e48e1363b78abebbf262235129db29ac0699f8d9
mhadev0703 <minchulha0703@gmail.com>
2024-09-05 23:58:56 -0400

    Add constants library

---
+++ app/lib/constants.ts
@@ -0,0 +1,12 @@
+// React Query related constants
+export const COINS_PER_PAGE = 50;
+export const REFRESH_INTERVAL = 30 * 60 * 1000; // 30 minutes in milliseconds
+
+// Color constants for themes
+export const COLORS = {
+  primary: '#0fbcf9',
+  secondary: '#9c88ff',
+  positive: 'green',
+  negative: 'red',
+};
+
