15ab584e069db9ec9e70c5cf43809d380a163f1d
mhadev0703 <minchulha0703@gmail.com>
2024-09-08 23:56:16 -0400

    Add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,25 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0fbcf9',
+        secondary: '#9c88ff',
+        positive: 'green',
+        negative: 'red',
+        lightBg: '#ffffff',
+        darkBg: '#2f3640',
+        lightText: '#000000',
+        darkText: '#ffffff',
+      }
+    },
   },
   plugins: [],
 };
