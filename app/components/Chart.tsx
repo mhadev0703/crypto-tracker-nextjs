e60f74b31100f0009c19f5a70ee07041f7844df7
mhadev0703 <minchulha0703@gmail.com>
2024-09-10 23:57:02 -0400

    Make chart rendering as separate component

---
+++ app/components/Chart.tsx
@@ -0,0 +1,64 @@
+import { useCoinHistory } from "../hooks/useCoinData";
+import dynamic from "next/dynamic";
+
+const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+export default function Chart({ coinId }: { coinId: string }) {
+  console.log("Passed coinId: ", coinId);
+  const { data, isLoading, error } = useCoinHistory(coinId);
+
+  if (isLoading) return <p className="text-center py-5">Loading...</p>;
+  if (error) return <p className="text-center py-5">Error loading coin history: {error.message}</p>;
+
+  const processedData = data?.map((entry) => ({
+    x: new Date(entry[0]),
+    y: entry[4],
+  })) ?? [];
+
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: processedData,
+            },
+          ]}
+          options={{
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: { show: false },
+            xaxis: {
+              axisBorder: { show: true },
+              axisTicks: { show: true },
+              labels: { show: true },
+              type: "datetime",
+              categories: data?.map((price) => price[0]),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: { formatter: (value) => `$${value.toFixed(2)}` },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
