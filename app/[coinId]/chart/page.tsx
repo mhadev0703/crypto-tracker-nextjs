8f1557401949a99feced7f85d25785602552ef3f
mhadev0703 <minchulha0703@gmail.com>
2024-09-11 23:57:41 -0400

    Simplify entire code by importing chart props

--- app/[coinId]/chart/page.tsx
+++ app/[coinId]/chart/page.tsx
@@ -1,69 +1,11 @@
 "use client";
 
+import Chart from "../../components/Chart";
 import { useParams } from "next/navigation";
-import { useCoinHistory } from "@/app/hooks/useCoinData";
-import dynamic from "next/dynamic";
 
-const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
-
-export default function Chart() {
+export default function ChartPage() {
   const params = useParams();
   const coinId = params.coinId as string;
-  console.log("Passed coinId: ", coinId);
-  const { data, isLoading, error } = useCoinHistory(coinId);
 
-  if (isLoading) return <p className="text-center py-5">Loading...</p>;
-  if (error) return <p className="text-center py-5">Error loading coin history: {error.message}</p>;
-
-  const processedData = data?.map((entry) => ({
-    x: new Date(entry[0]),
-    y: entry[4],
-  })) ?? [];
-
-  return (
-    <div>
-      {isLoading ? (
-        "Loading chart..."
-      ) : (
-        <ApexChart
-          type="line"
-          series={[
-            {
-              name: "Price",
-              data: processedData,
-            },
-          ]}
-          options={{
-            chart: {
-              height: 300,
-              width: 500,
-              toolbar: { show: false },
-              background: "transparent",
-            },
-            grid: { show: false },
-            stroke: {
-              curve: "smooth",
-              width: 4,
-            },
-            yaxis: { show: false },
-            xaxis: {
-              axisBorder: { show: true },
-              axisTicks: { show: true },
-              labels: { show: true },
-              type: "datetime",
-              categories: data?.map((price) => price[0]),
-            },
-            fill: {
-              type: "gradient",
-              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
-            },
-            colors: ["#0fbcf9"],
-            tooltip: {
-              y: { formatter: (value) => `$${value.toFixed(2)}` },
-            },
-          }}
-        />
-      )}
-    </div>
-  );
+  return <Chart coinId={coinId} />;
 }
